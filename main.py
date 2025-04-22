eec82790b0e70d20f39ad0d15c3fc075a407726d
Navanet <21113147@student.hindustanuniv.ac.in>
2025-04-22 15:49:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ st.set_page_config(
     page_title="Sniper Systems Chatbot",
     page_icon="💼",
     layout="centered",
+    initial_sidebar_state="expanded", theme="dark"
 )
 
 # ---------------------- Custom Styling ----------------------
